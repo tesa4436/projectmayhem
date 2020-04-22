7a15afdd7dd17a7753487c0c79e887b2045e8754
Tristanas <43069390+Tristanas@users.noreply.github.com>
2020-04-22 21:50:54 +0300

    Added missing models

---
+++ ProjectMayhem/ProjectMayhem/Models/LearningModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectMayhem.Models
+{
+    public class Topic
+    {
+        public string name;
+        public List<SubTopic> subTopics;
+    }
+
+    public class SubTopic
+    {
+        public string name;
+    }
+
+    public class LearningDay
+    {
+        public DateTime date;
+        public Topic topic;
+    }
+}
---
+++ ProjectMayhem/ProjectMayhem/Models/LearningViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectMayhem.Models
+{
+    public class SceduleViewModel
+    {
+
+    }
+
+
+}
